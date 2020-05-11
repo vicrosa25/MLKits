29ef3aa61629098d5083752b0d6530bb3c12a725
Victor Rosa <vicrosa25@gmail.com>
2020-05-11 19:29:11 +0200

    Recording observation Data

--- plinko/score.js
+++ plinko/score.js
@@ -1,5 +1,10 @@
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a balls drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+
+  console.log(outputs);
 }
 
 function runAnalysis() {
